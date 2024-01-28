a41aed9d783761eef379f6b9eab47d36cd8cfe24
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-27 21:40:40 -0300

    añadimos un poco de estilo a la imagen y empleadoRow

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-
+import "./Empleado.css"
 const EmpleadoAvatar = ({img}) => {
     return (
-        <img src={img} alt="foto perfil" className='w-25'/>
+        <img src={img} alt="foto perfil" className='empleado rounded-circle'/>
     );
 };
 
--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -3,12 +3,12 @@ import EmpleadoAvatar from './EmpleadoAvatar';
 
 const EmpleadoRow = ({ empleado }) => {
     return (
-        <div className='d-flex justify-content-center'>
+        <div className='d-flex align-items-center my-3'>
             <EmpleadoAvatar img={empleado.pic}></EmpleadoAvatar>
             <div>
                 <h4>{empleado.fullName}</h4>
                 <div className='d-flex'>
-                    <p>{empleado.title}</p>
+                    <p className='fs-3'>{empleado.title}</p>
                     <p className='ms-2 btn btn-primary fs-6'>{empleado.department}</p>
                 </div>
             </div>
